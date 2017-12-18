57dffe6a5d6259c2ca77571210489b32b8e9cd7a
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-12-18 10:31:29 -0500

    zhe

--- OrcMathGUI/src/zhehao/CatalogMaker.java
+++ OrcMathGUI/src/zhehao/CatalogMaker.java
@@ -1,15 +1,67 @@
 package zhehao;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class CatalogMaker {
 
-	private ArrayList<Ninja> ninjaarray=new ArrayList<Ninja>(); 
+	private static ArrayList<Ninja> ninjaarray=new ArrayList<Ninja>(); 
 	
 	
 	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
 		CatalogMaker cm= new CatalogMaker();
-		System.out.println(cm.getCSVcontent());
+		//System.out.println(cm.getCSVcontent());
+		
+		String s=in.nextLine();
+		boolean adding=true;
+		while(adding) {
+			ninjaarray.add(new Ninja(ninjaname(),ninjaDesc(),ninjaAge()));
+			System.out.println("Would you like to add another ninja to ur village?");
+			s=in.nextLine();
+			if(s.equalsIgnoreCase("no")) {
+				adding= false;
+			}
+		}
+		
+		
+		
+	}
+	public static String ninjaname() {
+		Scanner in= new Scanner(System.in);
+		System.out.println("would would is your ninja's name?");
+		String s=in.nextLine();
+		while(invalidresponse(s)) {
+			System.out.println("please enter a valid response.");
+		}
+			return s;
+		
+	}
+	public static String ninjaDesc() {
+		Scanner in= new Scanner(System.in);
+		System.out.println("what makes your ninja stands out?");
+		String s=in.nextLine();
+		while(invalidresponse(s)) {
+			System.out.println("please enter a valid response.");
+		}
+			return s;
+	}
+	public static int ninjaAge() {
+		Scanner in= new Scanner(System.in);
+		System.out.println("would would is your ninja's age");
+		String s=in.nextLine();
+		while(invalidresponse(s)   ) {
+			System.out.println("please enter a valid response.");
+		}
+			return Integer.parseInt(s);
+		
+	}
+	public static boolean invalidresponse(String x) {
+		if(x=="") {
+			return true;
+		}
+		return false;
 	}
 	public CatalogMaker() {
 		ninjaarray.add(new Ninja("ho","hoho",13));
@@ -22,5 +74,6 @@ public class CatalogMaker {
 		}
 		return data;
 	}
+	
 
 }
